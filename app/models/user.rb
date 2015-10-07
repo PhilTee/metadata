c6169ed83c232b3e271c8858b02527c33b1a3186
PhilTee <philtownes1@gmail.com>
2015-10-07 07:33:22 +0100

    User model updated to generate metadata

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
 class User < ActiveRecord::Base
   has_many :metavalues
   has_many :metakeys, through: :metavalues
-  
+
+  after_initialize :buildmetadata
+
+  def metadata
+    @metadata
+  end
+
+  #TODO: Need to capture changes to self.metadata to feed these back into ActiveRecord
+
+  private
+
+  def buildmetadata
+    @metadata = {};
+    self.metavalues.map { |v| @metadata[ v.metakey.name.parameterize.underscore.to_sym ] = v.value  }
+  end
+
 end
